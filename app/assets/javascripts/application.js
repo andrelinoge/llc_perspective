ad4d85f8b77512282fe1eef2e95bd95f4229cd48
Andre Linoge <andrelinoge87@gmail.com>
2017-10-16 09:44:53 -0700

    added public theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,17 @@
-//= require jquery3
+//= require "theme/vendor/jquery-2.2.4.min"
+//= require "theme/vendor/modernizr-2.8.3.min"
+//= require "theme/bootstrap.min"
+
+//= require "theme/owl.carousel.min"
+//= require "jquery.nivo.slider"
+//= require "home"
+//= require "theme/jquery.meanmenu.min"
+//= require "theme/wow.min"
+//= require "theme/plugins"
+//= require "theme/jquery.scrollUp.min"
+//= require "theme/isotope.pkgd.min"
+//= require "theme/jquery.counterup.min" 
+//= require "theme/waypoints.min" 
+//= require "theme/validator.min"
+
 //= require rails-ujs
-//= require popper
-//= require bootstrap
-//= require bootstrap-notify
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
-  
-   namespace :admin do
-    root 'dashboard#index'
 
-    resources :settings, only: [:index, :edit, :update]
+  scope "(:locale)" do
+    root 'welcome#index'
+    
+    namespace :admin do
+      root 'dashboard#index'
+
+      resources :settings, only: [:index, :edit, :update]
+    end
   end
 end
