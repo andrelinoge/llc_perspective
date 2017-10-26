f7f47464700b45543682b8390650a0af55c4303f
Andre Linoge <andrelinoge87@gmail.com>
2017-10-26 18:42:01 +0300

    changed password

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ApplicationController < ApplicationController
-  http_basic_authenticate_with name: "admin", password: "admin"
+  http_basic_authenticate_with name: "admin", password: "!Admin123"
 
   layout 'admin-application'
 end
